7e7cb09ba3493b7fc16a38cfbe0b2c6a10ae931d
wasifraza909 <wasifraza909@gmail.com>
2022-05-07 18:25:56 +0500

    Ready to deploy

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use(errorHandler);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-  app.use("*", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(
       path.resolve(__dirname, "../", "frontend", "build", "index.html")
     );
