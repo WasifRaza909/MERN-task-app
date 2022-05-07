d6bf750e141739549573ceeb265de2777ccb8edc
wasifraza909 <wasifraza909@gmail.com>
2022-05-07 18:36:03 +0500

    Ready to deploy

--- backend/server.js
+++ backend/server.js
@@ -24,11 +24,11 @@ app.use(errorHandler);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-  app.get("*", (req, res) => {
+  app.get("*", (req, res) =>
     res.sendFile(
       path.resolve(__dirname, "../", "frontend", "build", "index.html")
-    );
-  });
+    )
+  );
 } else {
   app.get("/", (req, res) => res.send("Please set to production"));
 }
