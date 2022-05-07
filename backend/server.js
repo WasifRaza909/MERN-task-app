7193920ceee5bac2561d8c86c5db892fbb06cc50
wasifraza909 <wasifraza909@gmail.com>
2022-05-07 18:09:02 +0500

    Ready to deploy

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,8 @@ const { errorHandler } = require("./middlewares/errorHandler");
 
 const app = express();
 
+const port = process.env.PORT || 5000;
+
 connectDB();
 
 app.use(cors());
@@ -18,8 +20,18 @@ app.use("/api/users", require("./routes/userRoutes"));
 
 app.use(errorHandler);
 
-const PORT = 5000;
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-app.listen(PORT, (req, res) => {
-  console.log(`Server is running on port ${PORT}`.green.underline);
+  app.use("*", (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    );
+  });
+} else {
+  app.get("/", (req, res) => res.send("Please set to production"));
+}
+
+app.listen(port, (req, res) => {
+  console.log(`Server is running on port ${port}`.green.underline);
 });
